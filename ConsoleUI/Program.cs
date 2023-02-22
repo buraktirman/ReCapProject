1ca0b514433d3bb214ba464ea0be96821eee2cde
buraktirman <buraktrman@gmail.com>
2023-02-22 19:10:02 +0300

    Core layer added. (9. Gün Ödev 1)

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,11 +9,46 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //CarTest();
+            //BrandTest();
+            //ColorTest();
+        }
+
+        private static void ColorTest()
+        {
+            ColorManager colorManager = new ColorManager(new EfColorDal());
+
+            colorManager.Add(new Color { Name = "Yellow" });
+            colorManager.Delete(new Color { Id = 5, Name = "Orange" });
+            colorManager.Update(new Color { Id = 1, Name = "Pink" });
+
+            foreach (var color in colorManager.GetAll())
+            {
+                Console.WriteLine(color.Name);
+            }
+
+            Console.WriteLine("---------------------------------------------");
+
+            foreach (var color in colorManager.GetById(4))
+            {
+                Console.WriteLine(color.Name);
+            }
+        }
+
+        private static void CarTest()
+        {
             CarManager carManager = new CarManager(new EfCarDal());
+
+            carManager.Add(new Car {BrandId = 3, ColorId = 3, ModelYear = "2020", Description = "Audi A6", DailyPrice = 4500 });
+            carManager.Delete(new Car { Id = 1002 });
+            carManager.Update(new Car { Id = 3, Description = "Mercedes C200", DailyPrice = 2400, ModelYear = "2018", ColorId = 4, BrandId = 4 });
+
             foreach (var car in carManager.GetCarsByColorId(2))
             {
                 Console.WriteLine(car.Description);
             }
+
+            Console.WriteLine("---------------------------------------------");
 
             Car newCar = new Car { BrandId = 4, ColorId = 5, DailyPrice = 2500, ModelYear = "2023", Description = "Mercedes C180" };
             carManager.Add(newCar);
@@ -23,7 +58,36 @@ namespace ConsoleUI
                 Console.WriteLine(car.Description);
             }
 
-            Color color = new Color { Name = "Pink" };
+            foreach (var car in carManager.GetCarsByColorId(3))
+            {
+                Console.WriteLine(car.Description);
+            }
+
+            foreach (var car in carManager.GetCarsByBrandId(5))
+            {
+                Console.WriteLine(car.Description);
+            }
+        }
+
+        private static void BrandTest()
+        {
+            BrandManager brandManager = new BrandManager(new EfBrandDal());
+
+            brandManager.Add(new Brand { Name = "Range Rover" });
+            brandManager.Delete(new Brand { Id = 1002, Name = "Range Rover" });
+            brandManager.Update(new Brand { Id = 4, Name = "Mercedes Benz" });
+
+            foreach (var brand in brandManager.GetAll())
+            {
+                Console.WriteLine(brand.Name);
+            }
+
+            Console.WriteLine("---------------------------------------------");
+
+            foreach (var brand in brandManager.GetById(4))
+            {
+                Console.WriteLine(brand.Name);
+            }
         }
     }
 }
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,4 +1,5 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -9,54 +10,8 @@ using System.Text;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal : ICarDal
+    public class EfCarDal : EfEntityRepositoryBase<Car, ReCapProjectContext>, ICarDal
     {
-        public void Add(Car entity)
-        {
-            using (ReCapProjectContext context = new ReCapProjectContext())
-            {
-                var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Car entity)
-        {
-            using (ReCapProjectContext context = new ReCapProjectContext())
-            {
-                var deletedEntity = context.Entry(entity);
-                deletedEntity.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Car Get(Expression<Func<Car, bool>> filter)
-        {
-            using (ReCapProjectContext context = new ReCapProjectContext())
-            {
-                return context.Set<Car>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
-        {
-            using (ReCapProjectContext context = new ReCapProjectContext())
-            {
-                return filter == null
-                    ? context.Set<Car>().ToList()
-                    : context.Set<Car>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Car entity)
-        {
-            using (ReCapProjectContext context = new ReCapProjectContext())
-            {
-                var updatedEntity = context.Entry(entity);
-                updatedEntity.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
+        
     }
 }
